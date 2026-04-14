d1c98fbe218e9472ef806dd2729c99ac4b20abeb
Prashant Pandey <23prashantpandey@gmail.com>
2026-04-15 00:04:25 +0530

    Add files via upload

---
+++ test_variables.php
@@ -0,0 +1,13 @@
+<?php
+  $string = "Hello SGCA TECHNOLOGIES!";   // String
+  $integer = 2026;           // Integer
+  $float = 99.5;             // Float (Decimal)
+  $bool = true;              // Boolean
+  $array = array("AI", "IoT", "Web"); // Array
+
+  echo "String: " . $string . "<br>";
+  echo "Integer: " . $integer . "<br>";
+  echo "Float: " . $float . "<br>";
+  echo "Boolean: " . ($bool ? 'True' : 'False') . "<br>";
+  echo "Array element: " . $array[0];
+?>
