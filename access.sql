8c02884f108ec42e22cd148fe866552de9d0ee3d
Prashant Pandey <23prashantpandey@gmail.com>
2026-05-01 23:33:01 +0530

    Add files via upload

--- access.sql
+++ access.sql
@@ -1,1 +1,7 @@
-ALTER TABLE role_based_access_control ADD COLUMN role ENUM('admin', 'user') DEFAULT 'user';
+CREATE TABLE access (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') DEFAULT 'user'
+) ENGINE=InnoDB;
